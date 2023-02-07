656daf4737bdf7e967f086074ff5aa7ea584f08e
nikolay.dimitrov <nikolay.dimitrov@nexo.io>
2023-02-07 17:43:49 +0200

    Rename field in TransactionRepository

--- src/main/java/io/nexo/interview/transactions/repository/TransactionRepository.java
+++ src/main/java/io/nexo/interview/transactions/repository/TransactionRepository.java
@@ -1,7 +1,6 @@
 package io.nexo.interview.transactions.repository;
 
 import static io.nexo.interview.transactions.model.Constants.LOGGED_USER;
-import io.nexo.interview.transactions.model.Currency;
 import io.nexo.interview.transactions.model.Transaction;
 import io.nexo.interview.transactions.model.TransactionType;
 import org.springframework.stereotype.Component;
@@ -14,19 +13,19 @@ import java.util.UUID;
 public class TransactionRepository {
 
     public List<Transaction> findAll() {
-        List<Transaction> ledger = new ArrayList<>();
-        ledger.add(new Transaction(LOGGED_USER, 1, TransactionType.DEPOSIT, new BigDecimal("0.3")));
-        ledger.add(new Transaction(UUID.randomUUID(), 1, TransactionType.DEPOSIT, new BigDecimal("0.1")));
-        ledger.add(new Transaction(LOGGED_USER, 1, TransactionType.WITHDRAWAL, new BigDecimal("0.2")));
-        ledger.add(new Transaction(LOGGED_USER, 2, TransactionType.DEPOSIT, new BigDecimal("2")));
-        ledger.add(new Transaction(LOGGED_USER, 2, TransactionType.WITHDRAWAL, new BigDecimal("0.3")));
-        ledger.add(new Transaction(UUID.randomUUID(), 2, TransactionType.WITHDRAWAL, new BigDecimal("0.1")));
-        ledger.add(new Transaction(LOGGED_USER, 2, TransactionType.WITHDRAWAL, new BigDecimal("0.2")));
-        ledger.add(new Transaction(LOGGED_USER, 3, TransactionType.DEPOSIT, new BigDecimal("20000")));
-        ledger.add(new Transaction(UUID.randomUUID(), 3, TransactionType.WITHDRAWAL, new BigDecimal("2000")));
-        ledger.add(new Transaction(UUID.randomUUID(), 3, TransactionType.WITHDRAWAL, new BigDecimal("5000")));
-        ledger.add(new Transaction(UUID.randomUUID(), 3, TransactionType.DEPOSIT, new BigDecimal("2000")));
-        ledger.add(new Transaction(LOGGED_USER, 3, TransactionType.WITHDRAWAL, new BigDecimal("5000")));
-        return ledger;
+        List<Transaction> transactionLog = new ArrayList<>();
+        transactionLog.add(new Transaction(LOGGED_USER, 1, TransactionType.DEPOSIT, new BigDecimal("0.3")));
+        transactionLog.add(new Transaction(UUID.randomUUID(), 1, TransactionType.DEPOSIT, new BigDecimal("0.1")));
+        transactionLog.add(new Transaction(LOGGED_USER, 1, TransactionType.WITHDRAWAL, new BigDecimal("0.2")));
+        transactionLog.add(new Transaction(LOGGED_USER, 2, TransactionType.DEPOSIT, new BigDecimal("2")));
+        transactionLog.add(new Transaction(LOGGED_USER, 2, TransactionType.WITHDRAWAL, new BigDecimal("0.3")));
+        transactionLog.add(new Transaction(UUID.randomUUID(), 2, TransactionType.WITHDRAWAL, new BigDecimal("0.1")));
+        transactionLog.add(new Transaction(LOGGED_USER, 2, TransactionType.WITHDRAWAL, new BigDecimal("0.2")));
+        transactionLog.add(new Transaction(LOGGED_USER, 3, TransactionType.DEPOSIT, new BigDecimal("20000")));
+        transactionLog.add(new Transaction(UUID.randomUUID(), 3, TransactionType.WITHDRAWAL, new BigDecimal("2000")));
+        transactionLog.add(new Transaction(UUID.randomUUID(), 3, TransactionType.WITHDRAWAL, new BigDecimal("5000")));
+        transactionLog.add(new Transaction(UUID.randomUUID(), 3, TransactionType.DEPOSIT, new BigDecimal("2000")));
+        transactionLog.add(new Transaction(LOGGED_USER, 3, TransactionType.WITHDRAWAL, new BigDecimal("5000")));
+        return transactionLog;
     }
 }
